929f8d109b5fc815b04c46e2a0acccc500a26366
misha <blava1979@gmail.com>
2020-09-12 00:09:14 +0400

    Added posts in Home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import './Home.css'
 
 export default class Home extends Component {
-  render() {
+ state = {posts : [{}]}
+    async componentDidMount() {
+    const prms =  await axios.get('http://localhost:8000/')
+    const respcards = prms.data
+    this.setState({ posts : respcards})
+    console.log(this.state)
+    }
+    
+    
+    render() {
+      let rcards = this.state.posts
+      console.log(rcards)
+      let psts 
+      if (rcards)
+       psts = rcards.map((o, idx) => {
+        return  (<div key={idx}  className="col s12 m12">
+           <h2 className="header">{o.slug} </h2>
+            <div className="card horizontal">
+         <div className="card-image" >
+           <img  src={o.thumbnail}/>
+         </div>
+         <div className="card-stacked">
+           <div className="card-content">
+             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, et?</p>
+           </div>
+           <div className="card-action">
+             <a href="#">{o.tags}</a>
+           </div>
+         </div>
+       </div>
+  </div>)})
     return (
-      <div>
-        <img src="images/pen_bgr.jpg" className="pen-background" />
-       
-      </div>
-    )
-  }
+      <div className="container">
+        <div className="row">
+         {psts}
+         </div>
+    </div>
+    )}
 }
